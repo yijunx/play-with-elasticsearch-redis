f99422f9c1ccf879841aed8e756e99d4b1b1e3f8
yijunx <yijunx@umich.edu>
2025-02-03 07:13:49 +0000

    well i am going to restart this thing

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # play-with-elasticsearch-redis
 for cache layer before go into weaviate
+
+
+Some technical details regarding redis + elasticsearch: (need to verify, find out the best way to use it, to save our token usage to reduce gpu burden or $ cost)
+You can start by asking chatgpt:
+i want to use redis and elastic search the cache the result from queries. before redis and elastic search, i have use rag with LLM and vector db. i also want to enable that, there is segregation in redis for each user (i cannot get cache for Alice even if Alice asked same question, there might be privacy issue). how should I do?
+but user may vary the query even if it is asking the same thing. how about let redis have a key, and the key has a mapping label in elasticsearch, when the query comes, somehow i turn the query into label, and get the key back from elasticsearch. how do i do this? is this similar to inverted index?
+when do I do cache invalidation? any good suggestions?
+could you elaborate on the preprocess? 
+how do I get the tokens into elasticsearch? and then next query comes, how do i take the new token from this query then get the catch_id from elasticsearch?
---
+++ app/llm/basic_llm.py
@@ -0,0 +1,95 @@
+from typing import Iterable
+
+
+from openai import Client
+
+class MessageForLLM:
+    def __init__(self, role: str, content: str):
+        self.role = role
+        self.content = content
+
+    def model_dump(self):
+        return {"role": self.role, "content": self.content}
+
+
+class BasicLLM:
+    def __init__(self, base_url: str, api_key: str, model: str):
+        self.base_url = base_url
+        self.api_key = api_key
+        self.model_name = model
+        self.client = Client(base_url=self.base_url, api_key=self.api_key)
+
+    def stream_response(
+        self,
+        conversation: list[MessageForLLM],
+        sys_prompt: str,
+        temperature: int,
+        frequency_penalty: float = None,
+    ) -> Iterable[str]:
+        messages = [
+            {
+                "role": "system",
+                "content": sys_prompt,
+            },
+        ] + [x.model_dump() for x in conversation]
+
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=messages,
+            temperature=temperature,
+            frequency_penalty=frequency_penalty,
+            stream=True,
+        )
+        for chunk in response:
+            chunk_message = chunk.choices[0].delta.content
+            if chunk_message:
+                yield chunk_message
+
+    def full_response(
+        self,
+        conversation: list[MessageForLLM],
+        sys_prompt: str,
+        temperature: int,
+        frequency_penalty: float = None,
+    ) -> str:
+        messages = [
+            {
+                "role": "system",
+                "content": sys_prompt,
+            },
+        ] + [x.model_dump() for x in conversation]
+
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=messages,
+            temperature=temperature,
+            frequency_penalty=frequency_penalty,
+        )
+        return response.choices[0].message.content
+
+    def get_response(
+        self,
+        sys_prompt: str = None,
+        user_prompt: str = None,
+        temperature: int = 0,
+        frequency_penalty: float = None,
+    ) -> str:
+
+        if not sys_prompt and not user_prompt:
+            raise ValueError(
+                "At least one of sys_prompt or user_prompt must be provided"
+            )
+
+        messages = []
+        if sys_prompt:
+            messages.append({"role": "system", "content": sys_prompt})
+        if user_prompt:
+            messages.append({"role": "user", "content": user_prompt})
+
+        response = self.client.chat.completions.create(
+            model=self.model_name,
+            messages=messages,
+            temperature=temperature,
+            frequency_penalty=frequency_penalty,
+        )
+        return response.choices[0].message.content
