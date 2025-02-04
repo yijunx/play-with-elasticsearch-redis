b3b005cc884dc53d523aebe3cb5deb1c92c786aa
yijunx <yijunx@umich.edu>
2025-02-04 07:18:52 +0000

    use main..

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,15 @@ from redis import Redis
 
 from app.context_providers.base_context_provider import JiberishContextProvider
 from app.llm.basic_llm import BasicLLM
+from app.main import (
+    get_context_provider_one,
+    get_context_provider_two,
+    get_es,
+    get_llm,
+    get_r,
+    get_s1,
+    get_s2,
+)
 from app.services.s1 import S1
 from app.services.s2 import S2
 from app.utils.config import env
@@ -11,78 +20,34 @@ from app.utils.config import env
 
 @pytest.fixture
 def context_provider_one():
-    return JiberishContextProvider(
-        facts=[
-            "Bananas are berries, but strawberries aren’t! Botanically, bananas fall under the berry category, while strawberries don’t because they develop from a flower with multiple ovaries.",
-            "Octopuses have three hearts. Two pump blood to the gills, while the third pumps it to the rest of the body. Their blood is also blue because it contains copper-based hemocyanin instead of iron-based hemoglobin.",
-            "Honey never spoils. Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still safe to eat. Honey’s low moisture content and acidic pH make it inhospitable to bacteria.",
-            "An astronaut's height increases in space. Without the compressive force of gravity, spinal discs expand, making astronauts up to 2 inches taller while in space. They return to normal height upon returning to Earth.",
-            "Venus rotates backward. It’s the only planet in our solar system that rotates in a clockwise (retrograde) direction, taking about 243 Earth days to complete a single rotation.",
-            "Wombat poop is cube-shaped. Wombats have specialized intestines that create cube-shaped droppings, helping them mark territory without their droppings rolling away.",
-            "Wombats dig extensive burrow systems. They have strong claws and powerful limbs that enable them to dig elaborate tunnels up to 30 meters long, which they use for shelter and protection.",
-            "A wombat’s pouch faces backward. Unlike many marsupials, wombats have a backward-facing pouch to prevent dirt from getting in when they dig.",
-            "Wombats can run up to 25 miles per hour. Despite their stubby legs and round bodies, they can sprint quite fast over short distances.",
-            "Sharks are older than trees. Sharks have been around for at least 400 million years, while the earliest trees appeared around 350 million years ago.",
-            "Cows have best friends. Studies show that cows have social connections and can feel stress when separated from their best buddies.",
-            "Mount Everest is not the tallest mountain from base to peak. Measured from its base on the seafloor, Mauna Kea in Hawaii is taller than Everest, reaching a total height of over 33,000 feet.",
-            "The inventor of the Pringles can was buried in one. Fred Baur, the inventor of the Pringles can, had some of his ashes buried in one of his iconic cans at his request.",
-        ],
-        simulated_delay=0.3,
-    )
+    return get_context_provider_one()
 
 
 @pytest.fixture
 def context_provider_two():
-    return JiberishContextProvider(
-        facts=[
-            "The Great Jagras is known for its ability to swallow prey whole, significantly increasing in size after feeding.",
-            "Rathalos, the 'King of the Skies,' is one of the most iconic monsters in the Monster Hunter series, known for its aerial attacks and fire breath.",
-            "The Palico companions in Monster Hunter are feline creatures that assist hunters in battle and gathering resources.",
-            "The Nergigante is a fearsome Elder Dragon known for its regenerative abilities and aggressive behavior.",
-            "The Monster Hunter series features a wide variety of weapon types, including the Great Sword, Long Sword, Bow, and Dual Blades.",
-            "The Anjanath is a large, bipedal monster resembling a T-Rex, known for its powerful fire attacks and aggressive nature.",
-            "The Zinogre is an electrifying monster that can charge itself with electricity to enhance its attacks.",
-            "The Monster Hunter series has a unique crafting system where players can create weapons and armor from the materials gathered from defeated monsters.",
-            "The Rajang is a powerful, ape-like monster known for its incredible strength and ability to shoot lightning from its mouth.",
-            "The Monster Hunter series has a dedicated fan base and has inspired various media adaptations, including an animated series and a live-action movie.",
-        ],
-        simulated_delay=0.5,
-    )
+    return get_context_provider_two()
 
 
 @pytest.fixture
 def llm():
-    return BasicLLM(
-        base_url=env.LLM_BASE_URL,
-        api_key=env.LLM_API_KEY,
-        model=env.LLM_NAME,
-    )
+    return get_llm()
 
 
 @pytest.fixture
 def r():
-    return Redis(
-        host=env.REDIS_HOST,
-        port=env.REDIS_PORT,
-        username=env.REDIS_USER,
-        password=env.REDIS_PASSWORD,
-        db=env.REDIS_DB,
-    )
+    return get_r()
 
 
 @pytest.fixture
 def es():
-    return Elasticsearch(
-        hosts=[f"http://{env.ELASTICSEARCH_HOST}:{env.ELASTICSEARCH_PORT}"],
-        # http_auth=(env.ELASTICSEARCH_USER, env.ELASTICSEARCH_PASSWORD),
-    )
+    get_es()
 
 
 @pytest.fixture
-def s2(context_provider_one, context_provider_two, llm):
-    return S2(llm=llm, context_providers=[context_provider_one, context_provider_two])
+def s2():
+    return get_s2()
 
 
 @pytest.fixture
-def s1(r, es):
-    return S1(redis=r, es=es, es_index=env.ELASTICSEARCH_INDEX)
+def s1():
+    return get_s1()
