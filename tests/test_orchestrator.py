dc2a364d1ebc12b2692f8debc239f2b8de8b7bc4
yijunx <yijunx@umich.edu>
2025-02-05 07:36:27 +0000

    lala

--- tests/test_orchestrator.py
+++ tests/test_orchestrator.py
@@ -1,8 +1,14 @@
 from app.orchestractor.base import Orchestrator
 
 
-def test_orchestractor(orchestractor: Orchestrator):
+def test_orchestractor1(orchestractor: Orchestrator):
     query = "where do i buy a gameboy"
+    answer = orchestractor.get_answer(query)
+    ans2 = orchestractor.get_answer(query)
+    assert answer == ans2
+
+def test_orchestractor2(orchestractor: Orchestrator):
+    query = "where do i buy a ps4"
     answer = orchestractor.get_answer(query)
     ans2 = orchestractor.get_answer(query)
     assert answer == ans2
--- tests/test_s2.py
+++ tests/test_s2.py
@@ -2,4 +2,4 @@ from app.services.s2 import S2
 
 
 def test_s2(s2: S2):
-    print(s2.get_answer(question="what lunch should I get"))
+    print(s2.get_answer(question="how do I cook a steak"))
